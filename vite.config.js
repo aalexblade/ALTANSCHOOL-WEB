d4c4f3f9cce6a696c4c465979c391cfd4b49b9a5
alex blade <aalexblade@gmail.com>
2025-05-23 08:14:40 +0300

    fix pjs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ALTANSCHOOL-WEB/', 
   plugins: [react()],
 });
